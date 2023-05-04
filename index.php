90c27306648fa02a3c7735424fe15c07432dc950
Richard Caceres <rchrdccrs@gmail.com>
2023-05-04 13:48:30 -0700

    Remove home link again

--- index.php
+++ index.php
@@ -9,143 +9,147 @@ $wf_json_file = "$file_web_path.json";
 
 if (!isset($_GET["file"])) {
 
-  ?>
-  <h1>Index of <?=$media_dir?></h1><hr/>
+?>
+  <h1>Index of <?= $media_dir ?></h1>
+  <hr />
   <?
 
-  function iterateDirectory($i) {
-      global $media_dir;
-      echo '<ul>';
-      foreach ($i as $path) {
-          if ($path->isDir()) {
-              iterateDirectory($path);
-          } else {
-            if (file_exists("$path.dat")) {
-              $relative_path = str_replace($media_dir, "", $path);
-              // echo $media_dir . "<BR>";
-              // echo $path . "<BR>";
-              // echo $relative_path . "<BR>";
-              echo "<li><a href='?file=".urlencode($relative_path)."'>$relative_path</a></li>";
-            }
-          }
+  function iterateDirectory($i)
+  {
+    global $media_dir;
+    echo '<ul>';
+    foreach ($i as $path) {
+      if ($path->isDir()) {
+        iterateDirectory($path);
+      } else {
+        if (file_exists("$path.dat")) {
+          $relative_path = str_replace($media_dir, "", $path);
+          // echo $media_dir . "<BR>";
+          // echo $path . "<BR>";
+          // echo $relative_path . "<BR>";
+          echo "<li><a href='?file=" . urlencode($relative_path) . "'>$relative_path</a></li>";
+        }
       }
-      echo '</ul>';
+    }
+    echo '</ul>';
   }
   $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($media_dir));
   iterateDirectory($iterator);
 
   exit;
-
 } else {
   ?>
-    <a href="?">
+  <!-- <a href="?">
       Home
-    </a>
-  <?
+    </a> -->
+<?
 
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?=$file?></title>
-    <link rel="stylesheet" href="main.css" />
-  </head>
-  <body>
-      <div id="waveform-container">
 
-        <div id="overview-container"></div>
-        <div id="zoomview-container"></div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title><?= $file ?></title>
+  <link rel="stylesheet" href="main.css" />
+</head>
 
+<body>
+  <div id="waveform-container">
+
+    <div id="overview-container"></div>
+    <div id="zoomview-container"></div>
+
+  </div>
+  <div class="container">
+
+    <div id="demo-controls">
+      <audio id="audio" controls="controls">
+        <source src="<?= $file_web_path ?>" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
+
+      <div id="controls">
+        <button class="hide" data-action="zoom-in">Zoom in</button>
+        <button class="hide" data-action="zoom-out">Zoom out</button>
+        <button class="hide" data-action="log-data">Log segments/points</button>
+        <input class="hide" type="text" id="seek-time" value="0.0">
+        <button class="hide" data-action="seek">Seek</button>
+        <label class="hide" for="amplitude-scale">Amplitude scale</label>
+        <input class="hide" type="range" id="amplitude-scale" min="0" max="10" step="1">
+        <input class="hide" type="checkbox" id="auto-scroll" checked>
+        <label class="hide" for="auto-scroll">Auto-scroll</label>
+        <button class="hide" data-action="resize">Resize</button>
+        <button class="hide" data-action="toggle-zoomview">Show/hide zoomable waveform</button>
+        <button class="hide" data-action="toggle-overview">Show/hide overview waveform</button>
+        <button class="hide" data-action="destroy">Destroy</button>
+        <button class="hide" data-action="save">Save</button>
       </div>
-    <div class="container">
-
-      <div id="demo-controls">
-        <audio id="audio" controls="controls">
-          <source src="<?=$file_web_path?>" type="audio/mpeg">
-          Your browser does not support the audio element.
-        </audio>
-
-        <div id="controls">
-                  <button class="hide" data-action="zoom-in">Zoom in</button>
-          <button class="hide" data-action="zoom-out">Zoom out</button>
-          <button class="hide" data-action="log-data">Log segments/points</button>
-          <input class="hide" type="text" id="seek-time" value="0.0">
-          <button class="hide" data-action="seek">Seek</button>
-          <label class="hide" for="amplitude-scale">Amplitude scale</label>
-          <input class="hide" type="range" id="amplitude-scale" min="0" max="10" step="1">
-          <input class="hide" type="checkbox" id="auto-scroll" checked>
-          <label class="hide" for="auto-scroll">Auto-scroll</label>
-          <button class="hide" data-action="resize">Resize</button>
-          <button class="hide" data-action="toggle-zoomview">Show/hide zoomable waveform</button>
-          <button class="hide" data-action="toggle-overview">Show/hide overview waveform</button>
-          <button class="hide" data-action="destroy">Destroy</button>
-          <button class="hide" data-action="save">Save</button>
-        </div>
-      </div>
-
-      <div>
-        <h1><?=$file?></h1>
-      </div>
-
-      <div id="comment-form">
-        <textarea id="comment-textarea" placeholder="Add a comment"></textarea>
-        <div class="comment-form-bottom">
-          <label>Add comment at current time <input id="include-time-field" type="checkbox" name="current-time" checked /></label>
-          <button data-action="add-point">Add Comment</button>
-          <button class="hide" data-action="add-segment">Add a Segment at current time</button>
-        </div>
-      </div>
-
-      <div class="log">
-
-        <div id="points" class="hide">
-          <h2>Comments</h2>
-          <ul id="points-container">
-          </ul>
-        </div>
-      </div>
-
-        <div id="segments" class="hide">
-          <h2>Segments</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Label</th>
-                <th>Start time</th>
-                <th>End time</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
-        </div>
-
     </div>
-    <script src="peaks.js"></script>
-    <script>
-      const FILE = <?=json_encode($file);?>;
-      var options = {
-        dataUri: {
-          arraybuffer: <?=json_encode($wf_dat_file)?>,
-        }
-      };
 
-      var request = new XMLHttpRequest();
-      request.open('GET', 'api/read.php?file=<?=urlencode($file)?>', false);
-      request.send(null);
+    <div>
+      <h1><?= $file ?></h1>
+    </div>
 
-      if (request.status === 200) {
-        console.log(request.responseText);
-        var serverData = JSON.parse(request.responseText);
-        Object.assign(options, serverData);
+    <div id="comment-form">
+      <textarea id="comment-textarea" placeholder="Add a comment"></textarea>
+      <div class="comment-form-bottom">
+        <label>Add comment at current time <input id="include-time-field" type="checkbox" name="current-time" checked /></label>
+        <button data-action="add-point">Add Comment</button>
+        <button class="hide" data-action="add-segment">Add a Segment at current time</button>
+      </div>
+    </div>
+
+    <div class="log">
+
+      <div id="points" class="hide">
+        <h2>Comments</h2>
+        <ul id="points-container">
+        </ul>
+      </div>
+    </div>
+
+    <div id="segments" class="hide">
+      <h2>Segments</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Label</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+  <script src="peaks.js"></script>
+  <script>
+    const FILE = <?= json_encode($file); ?>;
+    var options = {
+      dataUri: {
+        arraybuffer: <?= json_encode($wf_dat_file) ?>,
       }
-    </script>
-    <script src="main.js"></script>
-  </body>
+    };
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'api/read.php?file=<?= urlencode($file) ?>', false);
+    request.send(null);
+
+    if (request.status === 200) {
+      console.log(request.responseText);
+      var serverData = JSON.parse(request.responseText);
+      Object.assign(options, serverData);
+    }
+  </script>
+  <script src="main.js"></script>
+</body>
+
 </html>
